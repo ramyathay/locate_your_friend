e4f907f06fddc5b82d7ef09c2f1073e378a95a22
ramyathay <ramyatha.yugendernath@gmail.com>
2016-02-26 16:21:43 -0800

    Initial push to the repository

---
+++ locate_your_friend/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  locate_your_friend
+//
+//  Created by Ramyatha Yugendernath on 2/26/16.
+//  Copyright © 2016 Ramyatha Yugendernath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
